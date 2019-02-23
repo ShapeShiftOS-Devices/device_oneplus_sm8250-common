9f41a118b40a268fd5a1fb0f73482c1e56446df6
Arne Coucheron <arco68@gmail.com>
2019-02-23 03:28:02 +0000

    sm8250-common: Remove obsolete TARGET_BOARD_PLATFORM_GPU
    Change-Id: I40a14bce12ee4725af17a3e8fbc05143e5577f8e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,6 @@ TARGET_KERNEL_CONFIG := neutrino_defconfig
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
