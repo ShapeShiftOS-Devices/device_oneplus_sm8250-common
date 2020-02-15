8d51400068a1fd07d2b5d73a324f47181d0c34ff
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-14 22:39:33 -0500

    sm8250-common: Add custom audio amplifier for TFA amp feedback
     * Based on observing reverse engineering from davinci stock audio HAL.
     * Fixes speaker distortion on high volume.
    
     * Same logic with Smosia, rewritten by me.
    
    Co-authored-by: Smosia <stasenko_nv@nrcki.ru>
    Change-Id: I369555a9fcb06065177636b436d2e006c7a0b07d

--- common.mk
+++ common.mk
@@ -122,6 +122,7 @@ PRODUCT_PACKAGES += \
     audio.primary.kona \
     audio.r_submix.default \
     audio.usb.default \
+    audio_amplifier.kona \
     liba2dpoffload \
     libaudiopreprocessing \
     libbatterylistener \
