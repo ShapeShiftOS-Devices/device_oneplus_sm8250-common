979e21b8a60d6ac0455259d0b294e013c00703c5
Martinusbe <martinusbe@gmail.com>
2020-10-05 14:20:37 +0530

    sm8250-common: adapt the exact values for lockscreen charging info
    real time values
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,6 +55,9 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 BUILD_WITHOUT_VENDOR := true
