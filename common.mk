c83e1ce07d5b249cb5e8016c423dd2e11a412b37
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-04-01 20:12:39 +0530

    sm8250-common: Nuke existing DeviceSettings

--- common.mk
+++ common.mk
@@ -228,10 +228,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
-# Device Settings
-PRODUCT_PACKAGES += \
-    DeviceSettings
-
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
